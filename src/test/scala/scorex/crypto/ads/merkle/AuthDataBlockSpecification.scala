1fefa97b1c0278caaa4659e6eb01061f4cd941fc
kushti <kushtech@yahoo.com>
2016-03-18 09:51:35 +0300

    unused import fix

--- src/test/scala/scorex/crypto/ads/merkle/AuthDataBlockSpecification.scala
+++ src/test/scala/scorex/crypto/ads/merkle/AuthDataBlockSpecification.scala
@@ -3,7 +3,6 @@ package scorex.crypto.ads.merkle
 import org.scalacheck.Arbitrary
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 import org.scalatest.{Matchers, PropSpec}
-import scorex.crypto.hash.Blake2b256
 import scorex.utils.Random.randomBytes
 
 class AuthDataBlockSpecification extends PropSpec with PropertyChecks with GeneratorDrivenPropertyChecks with Matchers {
