5e702dac11291887c6fac0f9e5ccb7c0e9546a46
kushti <kushtech@yahoo.com>
2016-05-01 23:51:04 +0300

    FixedMerkleTree impl removed

--- src/main/scala/scorex/crypto/authds/merkle/fixed/FixedMerkleTree.scala
+++ src/main/scala/scorex/crypto/authds/merkle/fixed/FixedMerkleTree.scala
@@ -1,16 +1,9 @@
 package scorex.crypto.authds.merkle.fixed
 
 import scorex.crypto.authds.merkle.MerkleTree
-import scorex.crypto.authds.storage.{MvStoreStorageType, StorageType}
+import scorex.crypto.authds.storage.StorageType
 import scorex.crypto.hash.CryptographicHash
-
-
 
 trait FixedMerkleTree[HashFn <: CryptographicHash, ST <: StorageType] extends MerkleTree[HashFn, ST] {
   override val size: Long
-}
-
-
-abstract class MvStoreFixedMerkleTree[HashFn <: CryptographicHash](override val hashFunction: HashFn)
-  extends FixedMerkleTree[HashFn, MvStoreStorageType] {
 }
