8ce68be0864588a0979e4cd88570589e37afcc3b
catena <catena@protonmail.com>
2016-05-30 15:54:33 +0300

    Fix SLElement ordering

--- src/main/scala/scorex/crypto/crypto.scala
+++ src/main/scala/scorex/crypto/crypto.scala
@@ -28,6 +28,11 @@ package object crypto {
   }
 
   //TODO replace to fast implementation
-  def compare(a1: Array[Byte], a2: Array[Byte]): Int = BigInt((0: Byte) +: a1).compare(BigInt((0: Byte) +: a2))
+  def compare(a1: Array[Byte], a2: Array[Byte]): Int = {
+    if (a1.isEmpty && a2.isEmpty) 0
+    else if (a1.isEmpty) -1
+    else if (a2.isEmpty) 1
+    else BigInt(a1).compare(BigInt(a2))
+  }
 
 }
