9df931ec5381574f1a286d0259f7168319fa760b
catena <catena@protonmail.com>
2016-08-19 10:11:08 +0300

    More tests

--- src/test/scala/scorex/crypto/authds/sltree/SLTreeSpecification.scala
+++ src/test/scala/scorex/crypto/authds/sltree/SLTreeSpecification.scala
@@ -4,28 +4,52 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{Matchers, PropSpec}
 import scorex.crypto.TestingCommons
 import scorex.crypto.authds.binary.{SLTKey, SLTree}
+import scorex.utils.ByteArray
 
 
 class SLTreeSpecification extends PropSpec with GeneratorDrivenPropertyChecks with Matchers with TestingCommons {
 
 
-  property("SLTree insert one") {
+  property("SLTree special case") {
     val slt = new SLTree()
     val digest = slt.rootHash()
-    val key: SLTKey = Array.fill(32)(0: Byte)
-    val value: SLTKey = Array.fill(32)(1: Byte)
+    val key: SLTKey = Array.fill(32)(5: Byte)
+    val value: SLTKey = Array.fill(32)(5: Byte)
     val (success, proof) = slt.insert(key, value)
     success shouldBe true
     proof.verifyInsert(digest)._1 shouldBe true
+
+    val digest2 = slt.rootHash()
+    val key2: SLTKey = Array.fill(32)(4: Byte)
+    require(ByteArray.compare(key2, key) < 0)
+
+    val value2: SLTKey = Array.fill(32)(4: Byte)
+    val (success2, proof2) = slt.insert(key, value)
+    success2 shouldBe true
+    proof2.verifyInsert(digest2)._1 shouldBe true
+
   }
 
-  /*
-    property("SLTree insert") {
-      forAll { (key: Array[Byte], value: Array[Byte]) =>
-
-
-      }
+  property("SLTree insert one") {
+    forAll { (key: Array[Byte], value: Array[Byte]) =>
+      val slt = new SLTree()
+      val digest = slt.rootHash()
+      val (success, proof) = slt.insert(key, value)
+      success shouldBe true
+      proof.verifyInsert(digest)._1 shouldBe true
     }
-  */
+  }
+
+  property("SLTree insert") {
+    val slt = new SLTree()
+    forAll { (key: Array[Byte], value: Array[Byte]) =>
+      val digest = slt.rootHash()
+      val (success, proof) = slt.insert(key, value)
+      success shouldBe true
+      proof.verifyInsert(digest)._1 shouldBe true
+      proof.copy(key = proof.key ++ Array(0: Byte)).verifyInsert(digest)._1 shouldBe false
+      proof.copy(value = proof.value ++ Array(0: Byte)).verifyInsert(digest)._1 shouldBe false
+    }
+  }
 
 }
