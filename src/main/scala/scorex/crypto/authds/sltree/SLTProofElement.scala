84d8d231c1fd3da27d773eaf2d25f8e5c1929e25
catena <catena@protonmail.com>
2016-08-25 12:57:51 +0300

    Bytes to proof elements

--- src/main/scala/scorex/crypto/authds/sltree/SLTProofElement.scala
+++ src/main/scala/scorex/crypto/authds/sltree/SLTProofElement.scala
@@ -1,13 +1,19 @@
 package scorex.crypto.authds.sltree
 
+import com.google.common.primitives.Ints
 import scorex.crypto.encode.Base58
 
-sealed trait SLTProofElement
+sealed trait SLTProofElement {
+  val bytes: Array[Byte]
+}
 
-case class SLTProofLevel(e: Int) extends SLTProofElement
+case class SLTProofLevel(e: Int) extends SLTProofElement {
+  val bytes: Array[Byte] = Ints.toByteArray(e)
+}
 
 trait SLTProofLabel extends SLTProofElement {
   val e: Array[Byte]
+  val bytes: Array[Byte] = e
 
   override def toString: String = s"SLTProofKey(${Base58.encode(e).take(8)})"
 }
@@ -17,9 +23,11 @@ case class SLTProofRightLabel(e: Array[Byte]) extends SLTProofLabel
 case class SLTProofLeftLabel(e: Array[Byte]) extends SLTProofLabel
 
 case class SLTProofKey(e: SLTKey) extends SLTProofElement {
+  val bytes: Array[Byte] = e
   override def toString: String = s"SLTProofKey(${Base58.encode(e).take(8)})"
 }
 
 case class SLTProofValue(e: SLTValue) extends SLTProofElement {
+  val bytes: Array[Byte] = e
   override def toString: String = s"SLTProofKey(${Base58.encode(e).take(8)})"
 }
