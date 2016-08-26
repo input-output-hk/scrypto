861cd54db41432e2b5e0db2d09a5a3991f7112d7
catena <catena@protonmail.com>
2016-08-26 10:32:52 +0300

    Fix tests

--- src/test/scala/scorex/crypto/authds/sltree/SLTreeSpecification.scala
+++ src/test/scala/scorex/crypto/authds/sltree/SLTreeSpecification.scala
@@ -5,13 +5,10 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{Matchers, PropSpec}
 import scorex.crypto.TestingCommons
 
-import scala.util.Random
-
 
 class SLTreeSpecification extends PropSpec with GeneratorDrivenPropertyChecks with Matchers with TestingCommons {
 
-
-  property("SLTree modify") {
+  property("SLTree stream") {
     val slt = new SLTree()
     slt.insert(Array(0: Byte), _ => Longs.toByteArray(Long.MaxValue))
     var digest: Array[Byte] = slt.rootHash()
@@ -21,44 +18,19 @@ class SLTreeSpecification extends PropSpec with GeneratorDrivenPropertyChecks wi
     forAll { (sender: Array[Byte], recipient: Array[Byte], amount: Long) =>
       whenever(sender.nonEmpty && recipient.nonEmpty && amount >= 0) {
         //proover
-        //change sender balance
         val (_, senderProof: SLTModifyingProof) = slt.modify(sender, updateFunction(-amount))
-        //change recipient balance
-        val (_, recipientProof: SLTModifyingProof) = slt.modify(sender, updateFunction(amount))
+        val (_, recipientProof: SLTModifyingProof) = slt.modify(recipient, updateFunction(amount))
 
         //verifier
-        //check sender proof
         senderProof.key shouldBe sender
         digest = senderProof.verify(digest, updateFunction(-amount)).get
 
-        //check recipient proof
         recipientProof.key shouldBe recipient
         digest = recipientProof.verify(digest, updateFunction(amount)).get
-
       }
     }
   }
 
-  property("SLTree stream") {
-    val slt = new SLTree()
-    var digest: Array[Byte] = slt.rootHash()
-    var keys: Seq[Array[Byte]] = Seq()
-
-    forAll { (key: Array[Byte], value: Array[Byte], newVal: Array[Byte]) =>
-      whenever(key.nonEmpty && value.nonEmpty && newVal.nonEmpty && slt.lookup(key)._1.isEmpty) {
-        keys = key +: keys
-        val (success, proof) = slt.insert(key, _ => value)
-        success shouldBe true
-        digest = proof.verify(digest, _ => value).get
-
-        val uKey = keys(Random.nextInt(keys.length))
-        val (successUpdate, updateProof) = slt.update(uKey, _ => newVal)
-        successUpdate shouldBe true
-        slt.lookup(uKey)._1.get shouldBe newVal
-        digest = updateProof.verify(digest, _ => newVal).get
-      }
-    }
-  }
 
   property("SLTree insert one") {
     forAll {
@@ -152,7 +124,6 @@ class SLTreeSpecification extends PropSpec with GeneratorDrivenPropertyChecks wi
     }
   }
 
-
   property("SLTree update") {
     val slt = new SLTree()
     forAll { (key: Array[Byte], value: Array[Byte], newVal: Array[Byte]) =>
@@ -163,14 +134,15 @@ class SLTreeSpecification extends PropSpec with GeneratorDrivenPropertyChecks wi
         proof.verify(digest, _ => value).isDefined shouldBe true
 
         val digest2 = slt.rootHash()
-        val (successUpdate, updateProof) = slt.update(key, _ => newVal)
+        val (successUpdate, updateProof) = slt.update(key, valueConcat(newVal))
         successUpdate shouldBe true
-        slt.lookup(key)._1.get shouldBe newVal
-        val newDigest = updateProof.verify(digest2, _ => newVal).get
+        slt.lookup(key)._1.get.take(newVal.length) shouldEqual newVal
+        val newDigest = updateProof.verify(digest2, valueConcat(newVal)).get
         newDigest shouldEqual slt.rootHash()
       }
     }
   }
 
+  def valueConcat(v: SLTValue) = (old: Option[SLTValue]) => old.map(o => v ++ o).getOrElse(v)
 
 }
