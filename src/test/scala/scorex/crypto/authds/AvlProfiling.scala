f3c01620f57f895f42edd94373bf30f2a53ec251
catena <catena@protonmail.com>
2016-09-23 10:51:34 +0000

    AVL profiling

--- src/test/scala/scorex/crypto/authds/AvlProfiling.scala
+++ src/test/scala/scorex/crypto/authds/AvlProfiling.scala
@@ -2,22 +2,16 @@ package scorex.crypto.authds
 
 import org.scalatest.Matchers
 import scorex.crypto.authds.avltree.AVLTree
-import scorex.crypto.authds.sltree.SLTree
-import scorex.crypto.authds.wtree._
-import scorex.crypto.hash.Blake2b256
 
 
-object PerformanceMeter extends App with TwoPartyTests with Matchers {
+object AvlProfiling extends App with TwoPartyTests with Matchers {
 
   val Step = 1000
-  val ToCalculate = 1000
+  val ToCalculate = 10000
 
   val avl = new AVLTree()
-  val wt = new WTree()
-  val treap = new WTree()(Blake2b256, Level.treapLevel)
-  val slt = new SLTree()
 
-  val structures = Seq("treap", "wt", "slt", "avl")
+  val structures = Seq("avl")
   println("size, " +
     structures.map(_ + "InsertTime").mkString(", ") + ", " +
     structures.map(_ + "VerifyTime").mkString(", ") + ", " +
@@ -30,17 +24,9 @@ object PerformanceMeter extends App with TwoPartyTests with Matchers {
     structures.map(_ + "DirectionN").mkString(", "))
   (0 until ToCalculate) foreach { i =>
     val elements = genElements(Step, i)
-    // wt
-    val wtStats: Seq[Float] = profileTree(wt, elements, wt.rootHash())
-    // treap
-    val treapStats: Seq[Float] = profileTree(wt, elements, wt.rootHash())
     // avl
     val avlStats = profileTree(avl, elements, avl.rootHash())
-    //slt
-    val sltStats = profileTree(slt, elements, slt.rootHash())
-
-    println(s"${i * Step}, " +
-      wtStats.indices.map(i => treapStats(i) + ", " + wtStats(i) + ", " + sltStats(i) + ", " + avlStats(i)).mkString(", "))
+    println(s"${i * Step}, " + avlStats.indices.map(i => avlStats(i)).mkString(", "))
   }
 
 
