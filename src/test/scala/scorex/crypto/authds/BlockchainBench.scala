f623f16bad5ff03439710bd44f77854633f5d028
kushti <alex.chepurnoy@iohk.io>
2016-10-24 01:11:02 +0300

    separation in blockchain benchmark; mapdb for full verifier

--- src/test/scala/scorex/crypto/authds/BlockchainBench.scala
+++ src/test/scala/scorex/crypto/authds/BlockchainBench.scala
@@ -1,22 +1,18 @@
 package scorex.crypto.authds
 
 
-import scorex.crypto.authds.avltree.AVLTree
+import org.mapdb.{DBMaker, Serializer}
+import scorex.crypto.authds.TwoPartyDictionary.Label
+import scorex.crypto.authds.avltree.{AVLModifyProof, AVLTree}
 import scorex.crypto.authds.treap._
 import scorex.crypto.hash.Blake2b256Unsafe
-
-import scala.reflect.io.File
 import scala.util.{Random, Try}
 
-/*
-trait TwoPartyCommons extends UpdateF[TreapValue] {
-  val avl = new AVLTree(32)
 
+trait BenchmarkCommons {
   val hf = new Blake2b256Unsafe()
 
-  val db = DBMaker
-    .fileDB("/tmp/proofs")
-    .make()
+  val initElements = 5000000
 
   val blocks = 1000000
 
@@ -25,6 +21,15 @@ trait TwoPartyCommons extends UpdateF[TreapValue] {
   //val removalsInBlock = 150
 
   val perBlock = additionsInBlock + modificationsInBlock
+}
+
+trait TwoPartyCommons extends BenchmarkCommons with UpdateF[TreapValue] {
+  val avl = new AVLTree(32)
+
+  val db = DBMaker
+    .fileDB("/tmp/proofs")
+    .fileMmapEnable()
+    .make()
 
   val proofsMap = db.treeMap("proofs")
     .keySerializer(Serializer.LONG)
@@ -32,22 +37,25 @@ trait TwoPartyCommons extends UpdateF[TreapValue] {
     .createOrOpen()
 
   def set(value: TreapValue): UpdateFunction = { oldOpt: Option[TreapValue] => Try(Some(oldOpt.getOrElse(value))) }
+
   val balance = Array.fill(8)(0: Byte)
   val bfn = set(balance)
 }
 
-trait Initializing extends TwoPartyCommons {
-  val initElements = 5000000
+trait Initializing extends BenchmarkCommons {
   val keyCacheSize = 10000
 
   protected def initStep(i: Int): hf.Digest
 
-  protected def afterInit():Unit
+  protected def afterInit(): Unit
 
+  protected var keyCache: Array[hf.Digest] = null
 
-  (0 until initElements - keyCacheSize).foreach(initStep)
-  val keyCache = ((initElements - keyCacheSize) until initElements).map(initStep).toArray
-  afterInit()
+  def init(): Unit = {
+    (0 until initElements - keyCacheSize).foreach(initStep)
+    keyCache = ((initElements - keyCacheSize) until initElements).map(initStep).toArray
+    afterInit()
+  }
 }
 
 class Prover extends TwoPartyCommons with Initializing {
@@ -57,11 +65,11 @@ class Prover extends TwoPartyCommons with Initializing {
     k
   }
 
-  override protected def afterInit():Unit = {
+  override protected def afterInit(): Unit = {
   }
 
   //proofs generation
-  def dumpProofs(blockNum:Int) = {
+  def dumpProofs(blockNum: Int): Unit = {
     val proofs = (0 until additionsInBlock).map { i =>
       val k = hf("0" + i + ":" + blockNum)
       avl.modify(k, bfn).get
@@ -69,46 +77,56 @@ class Prover extends TwoPartyCommons with Initializing {
       val k = keyCache(Random.nextInt(keyCache.length))
       avl.modify(k, bfn).get
     }
+
+    var idx: Long = initElements + perBlock * blockNum
+    proofs.foreach { proof =>
+      proofsMap.put(idx, proof.bytes)
+      idx = idx + 1
+    }
   }
 }
 
 class Verifier extends TwoPartyCommons {
+  def checkProofs(blockNum: Int, rootValueBefore: Label): Label = {
+    var idx: Long = initElements + perBlock * blockNum
+    var root = rootValueBefore
+
+    while (idx < initElements + perBlock * (blockNum + 1)) {
+      val proof = proofsMap.get(idx)
+      root = AVLModifyProof.parseBytes(proof).get.verify(root, bfn).get
+      idx = idx + 1
+    }
+    root
+  }
 }
 
-class FullWorker extends TwoPartyCommons with Initializing {
-  val store = MVStore.open("/tmp/dbmvstore" + Random.nextInt())
-  store.setCacheSize(1)
+class FullWorker extends BenchmarkCommons with Initializing {
+  val store = DBMaker.fileDB("/tmp/fulldb").make()
 
-  val map = store.openMap[Array[Byte], Long]("balances")
+  val map = store.treeMap("proofs")
+    .keySerializer(Serializer.BYTE_ARRAY)
+    .valueSerializer(Serializer.INTEGER)
+    .createOrOpen()
 
   override protected def initStep(i: Int) = {
     if (i % 10000 == 0) println("init: i = " + i)
-    map.put(hf.hash(i + "-0"), 0)
+    val k = hf.hash(i + "-0")
+    map.put(k, 0)
+    k
   }
 
-  override protected def afterInit():Unit = {
+  override protected def afterInit(): Unit = {
     store.commit()
   }
-}
 
-
-object BlockchainBench extends App with TwoPartyCommons {
-  var size = initElements
-
-
-
-  (0 until blocks).foreach { b =>
-    size = size + additionsInBlock
-
-    val sf0 = System.currentTimeMillis()
+  def processBlock(blockNum: Int): Unit = {
     (0 until additionsInBlock).foreach { k =>
       //val size = map.size()
-      map.put(hf.hash(s"$k -- $b"), 0)
+      map.put(hf.hash(s"$k -- $blockNum"), 0)
     }
 
     (0 until modificationsInBlock).foreach { _ =>
-      val size = map.size()
-      val k = map.getKey(Random.nextInt(size - 100) + 1)
+      val k = keyCache(Random.nextInt(keyCache.length))
       map.put(k, map.get(k) + 100)
     }
 
@@ -119,6 +137,26 @@ object BlockchainBench extends App with TwoPartyCommons {
     }*/
 
     store.commit()
+  }
+}
+
+
+object BlockchainBench extends App with TwoPartyCommons {
+
+  val fw = new FullWorker
+  fw.init()
+  (1 to blocks).foreach{blockNum =>
+    val sf0 = System.currentTimeMillis()
+    fw.processBlock(blockNum)
+    val sf = System.currentTimeMillis()
+    val dsf = sf - sf0
+    println(s"block #$blockNum, full validation: $dsf")
+  }
+
+
+  /*
+    val sf0 = System.currentTimeMillis()
+
 
 
     var last100f = 0L
@@ -129,8 +167,6 @@ object BlockchainBench extends App with TwoPartyCommons {
     last100f = last100f + dsf
 
     val digest0 = avl.rootHash()
-
-
 
     //verification
     val sl0 = System.currentTimeMillis()
@@ -153,7 +189,6 @@ object BlockchainBench extends App with TwoPartyCommons {
       last100l = 0L
     }
   }
-
+*/
 
 }
-*/
