5e8378091215b582041fff77ec72463962d5dabe
catena <catena@protonmail.com>
2016-10-31 12:39:02 +0300

    BatchingBenchmark2

--- src/test/scala/scorex/crypto/authds/benchmarks/BatchingBenchmark.scala
+++ src/test/scala/scorex/crypto/authds/benchmarks/BatchingBenchmark.scala
@@ -7,31 +7,67 @@ import scorex.utils.Random
 
 object BatchingBenchmark extends App with TwoPartyTests {
 
-  val Step = 1000
-  val InitilaMods = 0
-  val NumMods = 2000000
+  val InitilaMods = 100000
+  val NumMods = 100000 + 4096 * 64
 
-  val oldProver = new oldProver(new AVLTree(32))
-
-  val newProver = new BatchAVLProver()
-  var digest = newProver.rootHash
+  var digest = Array[Byte]()
 
   var numInserts = 0
 
   val mods = generateModifications()
 
-  val initialModifications =  Modification.convert(mods.slice(0, InitilaMods))
-  oldProver.applyUpdates(initialModifications)
-  initialModifications foreach (m => newProver.performOneModification(m._1, m._2))
-  newProver.rootHash
-
+  println(s"NumInserts = $numInserts")
   println("Step, Plain size, GZiped size, Batched size, Old apply time, New apply time, Old verify time, New verify time")
-  (InitilaMods until(NumMods, Step)) foreach { i =>
+
+  bench2()
+
+  def bench2(): Unit = {
+
+    val steps = Seq(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 4096 * 2, 4096 * 4, 4096 * 8, 4096 * 16,
+      4096 * 32, 4096 * 64)
+
+    steps foreach { j =>
+      val oldProver = new oldProver(new AVLTree(32))
+      val newProver = new BatchAVLProver()
+
+      val initialModifications = Modification.convert(mods.slice(0, InitilaMods))
+      oldProver.applyUpdates(initialModifications)
+      initialModifications foreach (m => newProver.performOneModification(m._1, m._2))
+      newProver.generateProof
+      digest = newProver.rootHash
+      require(oldProver.rootHash sameElements digest)
+
+      oneStep(InitilaMods, j, j, oldProver, newProver)
+    }
+  }
+
+  def bench1(): Unit = {
+    val oldProver = new oldProver(new AVLTree(32))
+    val newProver = new BatchAVLProver()
+
+    val initialModifications = Modification.convert(mods.slice(0, InitilaMods))
+    oldProver.applyUpdates(initialModifications)
+    initialModifications foreach (m => newProver.performOneModification(m._1, m._2))
+    newProver.generateProof
+    digest = newProver.rootHash
+    require(oldProver.rootHash sameElements digest)
+
+    val Step = 1000
+    (InitilaMods until(NumMods, Step)) foreach { i =>
+      oneStep(i, Step, i, oldProver, newProver)
+    }
+  }
+
+
+  def oneStep(i: Int, step: Int, toPrint: Int, oldProver: oldProver, newProver: BatchAVLProver[_]): Unit = {
     System.gc()
-    val converted = Modification.convert(mods.slice(i, i + Step))
+    val converted = Modification.convert(mods.slice(i, i + step))
 
     val (oldProverTime, oldProves: Seq[AVLModifyProof]) = time {
-      oldProver.applyUpdates(converted).asInstanceOf[BatchSuccessSimple].proofs
+      oldProver.applyUpdates(converted) match {
+        case a: BatchSuccessSimple => a.proofs
+        case BatchFailure(e) => throw e
+      }
     }
     val oldBytes = oldProves.foldLeft(Array[Byte]()) { (a, b) =>
       a ++ b.proofSeq.map(_.bytes).reduce(_ ++ _)
@@ -45,8 +81,8 @@ object BatchingBenchmark extends App with TwoPartyTests {
       }
     }
 
-    val oldSize = oldBytes.length.toFloat / Step
-    val gzippedSize = Gzip.compress(oldBytes).length.toFloat / Step
+    val oldSize = oldBytes.length.toFloat / step
+    val gzippedSize = Gzip.compress(oldBytes).length.toFloat / step
 
     newProver.rootHash
     val (newProverTime, pf) = time {
@@ -54,7 +90,7 @@ object BatchingBenchmark extends App with TwoPartyTests {
       newProver.rootHash
       newProver.generateProof.toArray
     }
-    val newSize = pf.length.toFloat / Step
+    val newSize = pf.length.toFloat / step
 
     val newVerifier = new BatchAVLVerifier(digest, pf)
     newVerifier.digest.get shouldEqual digest
@@ -67,9 +103,8 @@ object BatchingBenchmark extends App with TwoPartyTests {
       newVerifier.digest
     }
     newVerifier.digest.get shouldEqual digest
-    println(s"$i,$oldSize,$gzippedSize,$newSize,$oldProverTime,$newProverTime,$oldVerifierTime,$newVerifierTime")
+    println(s"$toPrint,$oldSize,$gzippedSize,$newSize,$oldProverTime,$newProverTime,$oldVerifierTime,$newVerifierTime")
   }
-  print(s"NumInserts = $numInserts")
 
   def generateModifications(): Array[Modification] = {
     val mods = new Array[Modification](NumMods)
