a8ec39c7e0d0ea67b45111f14b5c962255388b64
kushti <alex.chepurnoy@iohk.io>
2016-11-01 20:06:20 +0300

    db name randomization fix

--- src/test/scala/scorex/crypto/authds/benchmarks/BlockchainBench.scala
+++ src/test/scala/scorex/crypto/authds/benchmarks/BlockchainBench.scala
@@ -28,7 +28,7 @@ trait BenchmarkCommons {
 
 trait TwoPartyCommons extends BenchmarkCommons with UpdateF[TreapValue] {
   lazy val db = DBMaker
-    .fileDB("/tmp/proofs" + Random.nextLong()) //todo: remove random
+    .fileDB("/tmp/proofs")
     .closeOnJvmShutdown()
     .make()
 
@@ -164,6 +164,7 @@ class BatchProver extends TwoPartyCommons with Batching with Initializing {
     rootsMap.put(blockNum, root)
 
     modificationKeys.zipWithIndex.foreach { case (mk, idx) =>
+
       modsMap.put(s"$blockNum--$idx", mk)
     }
     db.commit()
@@ -202,8 +203,7 @@ class BatchVerifier extends TwoPartyCommons with Batching {
 
     val modificationKeys = (0 until additionsInBlock + modificationsInBlock).map { idx =>
       idx -> modsMap.get(s"$blockNum--$idx")
-    }.toMap
-    println("v. : root before: " + Base58.encode(rootBefore))
+    }.toMap println("v. : root before: " + Base58.encode(rootBefore))
     (proof, rootBefore, rootAfter, modificationKeys)
   }
 
