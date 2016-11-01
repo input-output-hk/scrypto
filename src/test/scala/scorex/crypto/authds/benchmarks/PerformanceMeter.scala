1b03281759e411ceba344e1357081361d5f2d4c7
catena <catena@protonmail.com>
2016-11-01 10:09:56 +0300

    GC before every benchmark

--- src/test/scala/scorex/crypto/authds/benchmarks/PerformanceMeter.scala
+++ src/test/scala/scorex/crypto/authds/benchmarks/PerformanceMeter.scala
@@ -35,14 +35,15 @@ object PerformanceMeter extends App with TwoPartyTests {
     structures.map(_ + "BalanceN").mkString(", ") + ", " +
     structures.map(_ + "DirectionN").mkString(", "))
   (1 until ToCalculate) foreach { i =>
-    System.gc()
-
     val elements = genElements(Step, i, KL)
     // wt
+    System.gc()
     val wtStats: Seq[Float] = profileTree(wt, elements, wt.rootHash())
     // treap
+    System.gc()
     val treapStats: Seq[Float] = profileTree(treap, elements, treap.rootHash())
     // avl
+    System.gc()
     val avlStats = profileTree(avl, elements, avl.rootHash())
 
     println(s"${i * Step}, " +
