77edc8aeb00b0773f052f2f73657abb3e5919267
Leonid Reyzin <reyzin@bu.edu>
2016-11-01 23:44:16 -0400

    modified the benchmark to be more accurate

--- src/test/scala/scorex/crypto/authds/benchmarks/BatchingBenchmark.scala
+++ src/test/scala/scorex/crypto/authds/benchmarks/BatchingBenchmark.scala
@@ -7,31 +7,38 @@ import scorex.utils.Random
 
 object BatchingBenchmark extends App with TwoPartyTests {
 
-  val InitilaMods = 1000000
-  val NumMods = InitilaMods + 4096 * 64
+ 
 
-  var digest = Array[Byte]()
+  bench2
+  
+  // timeBenchmarksNewContinuous
+  //timeBenchmarksOldContinuous
+  //timeBenchmarksNew
+  //timeBenchmarksOld
 
-  var numInserts = 0
-
-  val mods = generateModifications()
-
-  println(s"NumInserts = $numInserts")
-  println("Step, Plain size, GZiped size, Batched size, Old apply time, New apply time, Old verify time, New verify time")
-
-  bench2()
 
   def bench2(): Unit = {
 
-    val steps = Seq(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 4096 * 2, 4096 * 4, 4096 * 8, 4096 * 16,
-      4096 * 32, 4096 * 64)
+    val InitialMods = 1000000
+    val NumMods = InitialMods + 4096 * 64
+    var digest = Array[Byte]()
+
+    val mods = generateModifications(NumMods)
+
+    println("Step, Plain size, GZiped size, Batched size, Old apply time, New apply time, Old verify time, New verify time")
+
+    val steps = Seq(64, 32, 16, 8, 4, 2, 1)
+
+
+//    val steps = Seq(1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 4096 * 2, 4096 * 4, 4096 * 8, 4096 * 16,
+ //     4096 * 32, 4096 * 64)
 
     steps foreach { j =>
       val oldProver = new oldProver(new AVLTree(32))
       val newProver = new BatchAVLProver()
 
-      val Step = InitilaMods / 1000
-      (0 until(InitilaMods, Step)) foreach { cur =>
+      val Step = InitialMods / 1000
+      (0 until(InitialMods, Step)) foreach { cur =>
         val initialModifications = Modification.convert(mods.slice(cur, cur + Step))
         oldProver.applyUpdates(initialModifications)
         initialModifications foreach (m => newProver.performOneModification(m._1, m._2))
@@ -40,15 +47,23 @@ object BatchingBenchmark extends App with TwoPartyTests {
         require(oldProver.rootHash sameElements digest)
       }
 
-      oneStep(InitilaMods, j, j, oldProver, newProver)
+      oneStep(InitialMods, j, j, 1024*32,oldProver, newProver, mods)
     }
   }
 
   def bench1(): Unit = {
+    val InitialMods = 1000000
+    val NumMods = InitialMods + 4096 * 64
+    var digest = Array[Byte]()
+
+    val mods = generateModifications(NumMods)
+
+    println("Step, Plain size, GZiped size, Batched size, Old apply time, New apply time, Old verify time, New verify time")
+    
     val oldProver = new oldProver(new AVLTree(32))
     val newProver = new BatchAVLProver()
 
-    val initialModifications = Modification.convert(mods.slice(0, InitilaMods))
+    val initialModifications = Modification.convert(mods.slice(0, InitialMods))
     oldProver.applyUpdates(initialModifications)
     initialModifications foreach (m => newProver.performOneModification(m._1, m._2))
     newProver.generateProof
@@ -56,69 +71,266 @@ object BatchingBenchmark extends App with TwoPartyTests {
     require(oldProver.rootHash sameElements digest)
 
     val Step = 1000
-    (InitilaMods until(NumMods, Step)) foreach { i =>
-      oneStep(i, Step, i, oldProver, newProver)
+    (InitialMods until(NumMods, Step)) foreach { i =>
+      oneStep(i, Step, i, i, oldProver, newProver, mods)
     }
   }
 
-
-  def oneStep(i: Int, step: Int, toPrint: Int, oldProver: oldProver, newProver: BatchAVLProver[_]): Unit = {
+  def oneStep(i: Int, step: Int, toPrint: Int, totalSize: Int, oldProver: oldProver, newProver: BatchAVLProver[_], mods: Array[Modification]): Unit = {
     System.gc()
-    val converted = Modification.convert(mods.slice(i, i + step))
-
-    val (oldProverTime, oldProves: Seq[AVLModifyProof]) = time {
-      oldProver.applyUpdates(converted) match {
-        case a: BatchSuccessSimple => a.proofs
-        case BatchFailure(e) => throw e
+    
+    var oldProverTime = 0.0
+    var newProverTime = 0.0
+    var oldVerifierTime = 0.0
+    var newVerifierTime = 0.0
+    var oldSize = 0.0
+    var newSize = 0.0
+    var gzippedSize = 0.0
+    
+    for (iteration <- 0 until (totalSize, step)) {   
+    
+      val converted = Modification.convert(mods.slice(i+iteration, i + iteration + step))
+      var digest = oldProver.rootHash
+      
+      
+      val (oldProverTimeT, (oldProofs: Seq[AVLModifyProof], oldBytes : Array[Byte])) = time {
+        oldProver.applyUpdates(converted) match {
+          case a: BatchSuccessSimple => 
+            oldProver.rootHash
+            // TODO: THIS IS NOT THE MOST EFFICIENCT SERIALIZATION,
+            // SO IT'S A BIT UNFAIR TO OLDPROVER
+            val oldBytes = a.proofs.foldLeft(Array[Byte]()) { (a, b) =>
+                 a ++ b.proofSeq.map(_.bytes).reduce(_ ++ _)
+            }
+            (a.proofs, oldBytes)
+          case BatchFailure(e) => throw e
+        }
       }
-    }
-    val oldBytes = oldProves.foldLeft(Array[Byte]()) { (a, b) =>
-      a ++ b.proofSeq.map(_.bytes).reduce(_ ++ _)
-    }
-    val oldVerifierTime = time {
-      var h = 0
-      oldProves.foldLeft(digest) { (prevDigest, proof) =>
-        val newDigest = proof.verify(prevDigest, converted(h)._2).get
-        h = h + 1
-        newDigest
-      }
-    }._1
+      
+      oldProverTime+=oldProverTimeT
+      
+      // TODO: THERE IS NO DESERIALIZATION
+      // SO IT'S A BIT TOO NICE TO OLDVERIFIER
+      val oldVerifierTimeT = time {
+        var h = 0
+        oldProofs.foldLeft(digest) { (prevDigest, proof) =>
+          val newDigest = proof.verify(prevDigest, converted(h)._2).get
+          h = h + 1
+          newDigest
+        }
+      }._1
+      
+      oldVerifierTime+=oldVerifierTimeT
 
-    val oldSize = oldBytes.length.toFloat / step
-    val gzippedSize = Gzip.compress(oldBytes).length.toFloat / step
+      oldSize = oldBytes.length.toFloat / step
+      gzippedSize = Gzip.compress(oldBytes).length.toFloat / step
 
-    newProver.rootHash
-    val (newProverTime, pf) = time {
-      converted foreach (m => newProver.performOneModification(m._1, m._2))
       newProver.rootHash
-      newProver.generateProof.toArray
+      val (newProverTimeT, pf) = time {
+        converted foreach (m => newProver.performOneModification(m._1, m._2))
+        newProver.rootHash
+        newProver.generateProof.toArray
+      }
+      newProverTime+=newProverTimeT
+      newSize = pf.length.toFloat / step
+
+      val (newVerifierTimeT, _) = time {
+        val newVerifier = new BatchAVLVerifier(digest, pf)
+        converted foreach (m => newVerifier.verifyOneModification(m._1, m._2))
+        newVerifier.digest
+      }
+      newVerifierTime += newVerifierTimeT 
+
     }
-    val newSize = pf.length.toFloat / step
-
-    val newVerifier = new BatchAVLVerifier(digest, pf)
-    newVerifier.digest.get shouldEqual digest
-
-    digest = oldProver.rootHash
-    assert(newProver.rootHash sameElements digest)
-
-    val (newVerifierTime, _) = time {
-      converted foreach (m => newVerifier.verifyOneModification(m._1, m._2))
-      newVerifier.digest
+    println(s"$toPrint,$oldSize,$gzippedSize,$newSize,${oldProverTime / totalSize},${newProverTime / totalSize}," +
+      s"${oldVerifierTime / totalSize},${newVerifierTime / totalSize}")
+  }
+  
+  def timeBenchmarksNewProverInsertOnly {
+    val newProver = new BatchAVLProver()
+    val numMods = 1024*1024
+    val batchSize = 1
+ 
+    var mods = new Array[Modification](1024)
+    for (i <- 0 until 1024) {
+      for (j <- 0 until 1024)
+        mods(j) = (Insert(Random.randomBytes(), Random.randomBytes(8)))      
+      val converted = Modification.convert(mods)
+      val (t, d) = time {
+        var j=0
+        while (j<1024) {
+          for (k <- 0 until batchSize)
+            newProver.performOneModification(converted(j+k)._1, converted(j+k)._2)
+          newProver.generateProof.toArray
+          newProver.rootHash
+          j+=batchSize
+        }
+      }
+      print(i)
+      print(" ")
+      println(t)
     }
-    newVerifier.digest.get shouldEqual digest
-    println(s"$toPrint,$oldSize,$gzippedSize,$newSize,${oldProverTime / step},${newProverTime / step}," +
-      s"${oldVerifierTime / step},${newVerifierTime / step}")
   }
 
-  def generateModifications(): Array[Modification] = {
+  def timeBenchmarksOldProverInsertOnly {
+    val oldProver = new AVLTree(32)
+    val numMods = 1024*1024
+    val batchSize = 1024
+ 
+    var mods = new Array[Modification](1024)
+    for (i <- 0 until 1024) {
+      for (j <- 0 until 1024)
+        mods(j) = (Insert(Random.randomBytes(), Random.randomBytes(8)))      
+      val converted = Modification.convert(mods)
+      val (t, d) = time {
+        var j=0
+        while (j<1024) {
+          for (k <- 0 until batchSize)
+            oldProver.modify(converted(j+k)._1, converted(j+k)._2).get.bytes
+          oldProver.rootHash
+          j+=batchSize
+        }
+      }
+      print(i)
+      print(" ")
+      println(t)
+    }
+  }
+
+
+  def timeBenchmarkNewProver {
+    val initialMods = 1024*1024
+    val totalSize = 32*1024
+    val measuredMods = 32*1024*20
+    val mods = generateModifications(initialMods+measuredMods)
+ 
+    
+    
+  /*  for (k<-0 until 10) { //NOTE: if you comment out this loop, the first few batches are slower by factor of 3-6
+      val (newProverTime, pf) = time {
+        var ctr = 0
+        while(ctr<1000) {
+          for (j<-0 until 1) {
+            val m = Modification.convert(mods(i))
+            newProver.performOneModification(m._1, m._2)
+            i+=1
+            ctr+=1
+          }
+          newProver.rootHash
+          newProver.generateProof.toArray
+        }
+      }
+      println(newProverTime)
+    }*/
+
+    println("batchSize,UnitCost")
+    var batchSize = totalSize
+    while (batchSize > 0) {
+    
+      val newProver = new BatchAVLProver()
+      for (i<-0 until initialMods) {
+        val m = Modification.convert(mods(i))
+        newProver.performOneModification(m._1, m._2)
+      }
+      newProver.rootHash // NOTE: if you comment out this line, the first batch becomes about 2 seconds slower
+      newProver.generateProof
+      
+      System.gc()
+
+      var i = initialMods
+
+      var numBatches = 0
+      val (newProverTime, pf) = time {
+        var ctr = 0
+        while(ctr<totalSize) {
+          for (j<-0 until batchSize) {
+            val m = Modification.convert(mods(i))
+            newProver.performOneModification(m._1, m._2)
+            i+=1
+            ctr+=1
+          }
+          newProver.rootHash
+          newProver.generateProof.toArray
+          numBatches += 1
+        }
+      }
+      print(batchSize)
+      print(",")
+      println(newProverTime/numBatches/batchSize)
+      batchSize/=2
+    }
+  }
+  
+  def timeBenchmarkOldProver {
+    val initialMods = 1024*1024
+    val totalSize = 32*1024
+    val measuredMods = 32*1024*20
+    val mods = generateModifications(initialMods+measuredMods)
+ 
+    
+    
+  /*  for (k<-0 until 10) { //NOTE: if you comment out this loop, the first few batches are slower by factor of 3-6
+      val (oldProverTime, pf) = time {
+        var ctr = 0
+        while(ctr<1000) {
+          for (j<-0 until 1) {
+            val m = Modification.convert(mods(i))
+            oldProver.modify(m._1, m._2).get.bytes
+            i+=1
+            ctr+=1
+          }
+          oldProver.rootHash
+        }
+      }
+      println(oldProverTime)
+    }*/
+
+    println("batchSize,UnitCost")
+    var batchSize = totalSize
+    while (batchSize > 0) {
+    
+      val oldProver = new AVLTree(32)
+      for (i<-0 until initialMods) {
+        val m = Modification.convert(mods(i))
+        oldProver.modify(m._1, m._2)
+      }
+      oldProver.rootHash // NOTE: if you comment out this line, the first batch becomes about 2 seconds slower
+      
+      System.gc()
+
+      var i = initialMods
+
+      var numBatches = 0
+      val (oldProverTime, pf) = time {
+        var ctr = 0
+        while(ctr<totalSize) {
+          for (j<-0 until batchSize) {
+            val m = Modification.convert(mods(i))
+            oldProver.modify(m._1, m._2).get.bytes
+            i+=1
+            ctr+=1
+          }
+          oldProver.rootHash
+          numBatches += 1
+        }
+      }
+      print(batchSize)
+      print(",")
+      println(oldProverTime/numBatches/batchSize)
+      batchSize/=2
+    }
+  }
+   
+
+
+  def generateModifications(NumMods : Int): Array[Modification] = {
     val mods = new Array[Modification](NumMods)
     mods(0) = Insert(Random.randomBytes(), Random.randomBytes(8))
 
     for (i <- 1 until NumMods) {
       if (i % 2 == 0) {
-        // with prob ~.5 insert a new one, with prob ~.5 update an existing one
+        // half inserts, half lookups
         mods(i) = Insert(Random.randomBytes(), Random.randomBytes(8))
-        numInserts += 1
       } else {
         val j = Random.randomBytes(3)
         mods(i) = Update(mods((j(0).toInt.abs + j(1).toInt.abs * 128 + j(2).toInt.abs * 128 * 128) % i).key, Random.randomBytes(8))
