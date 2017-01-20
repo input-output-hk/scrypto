771cc7d344c24d291e69854fefd33674c430858d
catena <catena@protonmail.com>
2017-01-20 17:06:13 +0300

    Add Skein512 tests

--- src/test/scala/scorex/crypto/hash/SkeinSpecification.scala
+++ src/test/scala/scorex/crypto/hash/SkeinSpecification.scala
@@ -12,8 +12,9 @@ class SkeinSpecification extends HashTest {
 
   hashCheckString(Skein512,
     Map(
-      "" -> "bc5b4c50925519c290cc634277ae3d6257212395cba733bbad37a4af0fa06af41fca7903d06564fea7a2d3730dbdb80c1f85562dfcc070334ea4d1d9e72cba7a"
-    //TODO
+      "" -> "bc5b4c50925519c290cc634277ae3d6257212395cba733bbad37a4af0fa06af41fca7903d06564fea7a2d3730dbdb80c1f85562dfcc070334ea4d1d9e72cba7a",
+      "The quick brown fox jumps over the lazy dog" -> "94c2ae036dba8783d0b3f7d6cc111ff810702f5c77707999be7e1c9486ff238a7044de734293147359b4ac7e1d09cd247c351d69826b78dcddd951f0ef912713",
+      "The quick brown fox jumps over the lazy dog." -> "658223cb3d69b5e76e3588ca63feffba0dc2ead38a95d0650564f2a39da8e83fbb42c9d6ad9e03fbfde8a25a880357d457dbd6f74cbcb5e728979577dbce5436"
     )
   )
 
