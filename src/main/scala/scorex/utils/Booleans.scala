5e43e7418e997846727577d4d074235a74b1e1fa
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2021-11-07 00:19:39 +0300

    build.sbt improved, unused code in avltree.scala removed

--- src/main/scala/scorex/utils/Booleans.scala
+++ src/main/scala/scorex/utils/Booleans.scala
@@ -2,8 +2,12 @@ package scorex.utils
 
 object Booleans {
 
-  def toByteArray(v: Boolean): Array[Byte] = if (v) Array(1: Byte) else Array(0: Byte)
+  lazy val trueArray = Array(1: Byte)
 
-  def fromByteArray(b: Array[Byte]): Boolean = if (b sameElements Array(1: Byte)) true else false
+  lazy val falseArray = Array(0: Byte)
+
+  def toByteArray(v: Boolean): Array[Byte] = if (v) trueArray else falseArray
+
+  def fromByteArray(b: Array[Byte]): Boolean = if (b sameElements trueArray) true else false
 
 }
