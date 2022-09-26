054277fb17ed1e6e3059393e0e0b5d7dfc47a3a0
Alexander Slesarenko <avslesarenko@gmail.com>
2022-09-26 16:39:47 +0200

    scala-js: move to JVM: Whirlpool

--- jvm/src/main/scala/scorex/crypto/hash/Whirlpool.scala
+++ jvm/src/main/scala/scorex/crypto/hash/Whirlpool.scala
@@ -2,9 +2,7 @@ package scorex.crypto.hash
 
 import org.bouncycastle.crypto.digests.WhirlpoolDigest
 
-
 object Whirlpool extends BouncyCastleHash[Digest64] with CryptographicHash64 {
-
   override protected lazy val digestFn = new WhirlpoolDigest
 
   override def hash(input: Message): Digest64 = Digest64 @@ internalHash(input)
